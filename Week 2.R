3f2047aa960f3ded1f8f4fe9cbf10daca4d13f52
Laxmi Jha <ljha@ucdavis.edu>
2024-10-03 14:05:35 -0700

    Week 2 Updated

---
+++ Week 2.R
@@ -0,0 +1,65 @@
+#Introductions R: mathematics
+# Incomplete Command 
+4+(8*3)^2
+#this is a comment
+#scientific notation
+2/100000
+4e3
+#mathematical function
+exp(1)
+exp(3)
+exp(4)
+log(4)
+sqrt(4)
+?log
+log(4,2)
+log(2,4)
+log(x-2,base=4)
+x<-1
+rm(x)
+#nested function
+
+sqrt(exp(40))
+lof(exp(100))
+#six comparison function
+mynumber<-6
+mynumber==5
+mynumber !=5
+
+
+
+mynumber>4
+munumber<3
+mynumber<=-1
+#objects name conventions
+mynumber<-7
+othernumber<- -3
+mynumber*othernumber
+#object name convention
+numSamples<-50
+num_samples<-40
+#tab completion
+rm(list=ls())
+#errors and warnings
+log_of_word<-log("a_word")
+log_of_word
+log_of_negative<-log(-2)
+log_of_negative
+
+
+
+#challenges
+
+
+
+
+
+
+elephant1_kg<-3492
+elephant2_lb<-7757
+
+
+elephant1_lb<-elephant1_kg*2.2
+
+elephant2_lb>elephant1_lb
+myelephants<-c(elephant1_lb,elephant2_lb)
